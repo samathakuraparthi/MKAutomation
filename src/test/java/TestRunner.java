5462702c5526e0326dbb60c1468e055dc81818f3
samatha <samathakuraparthi@gmail.com>
2023-03-18 14:17:30 +0000

    Updated reporting plugin

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -6,9 +6,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "classpath:features",
         glue = {"com/mk/stepDefinitions"},
         tags = "@mkTest",
-        plugin = {"pretty",
-                "html:target/site/cucumber-pretty",
-                "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/site/extent-reports/index.html"},
+        plugin = {"me.jvt.cucumber.report.PrettyReports:target/cucumber"},
         monochrome = true)
 public class TestRunner {
 
